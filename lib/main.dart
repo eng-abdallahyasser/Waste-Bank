33839213efe99adde1df243036d3b6bdc0ef04e0
eng-abdallahyasser <abdallahyasser1277@gmail.com>
2026-06-10 14:05:04 +0300

    feat: integrate Firebase core and configuration for the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:waste_bank/firebase_options.dart';
 import 'package:waste_bank/routes/app_pages.dart';
 import 'package:waste_bank/routes/app_routes.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await GetStorage.init();
 
   // Check if user is already logged in via stored token
